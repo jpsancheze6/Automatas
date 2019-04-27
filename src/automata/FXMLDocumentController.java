c249ae4219b92be5226ca8ddc14b18cfc1960246
José Sánchez <jose.venados6@gmail.com>
2019-04-27 00:29:18 -0600

    Graficación completada

--- src/automata/FXMLDocumentController.java
+++ src/automata/FXMLDocumentController.java
@@ -1,7 +1,12 @@
 package automata;
 
+import dk.brics.automaton.Automaton;
+import dk.brics.automaton.RegExp;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.RandomAccessFile;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -17,17 +22,27 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class FXMLDocumentController implements Initializable {
 
+    static String rutaDot = "C:\\Program Files (x86)\\Graphviz2.38\\bin\\dot.exe";//cambiar
+    static String textoBase = "codigo.txt"; //cambiar
+    static String imagen = "grafo.jpg";//cambiar
+    static String jp = "-Tjpg";
+    static String O = "-o";
+    
     @FXML
     private TextField txtExpresion, txtCadena;
     @FXML
     private Button btnComprobar;
     @FXML
-    private Label lblAutomata;
+    private Label lblAutomata, lblAutomataImagen;
+    @FXML
+    private ImageView imgAutomata;
 
     @FXML
     private void comprobarExpresion() throws IOException {
@@ -57,6 +72,8 @@ public class FXMLDocumentController implements Initializable {
                 raf.writeInt(texto.length());
                 raf.writeChars(texto);
                 raf.close();
+                //Crea el archivo jpg con el automata
+                expresion(texto);
                 try {
                     //Llamar a una nueva ventana
                     FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FXMLAutomata.fxml"));
@@ -90,6 +107,13 @@ public class FXMLDocumentController implements Initializable {
             }
             raf.close();
             lblAutomata.setText(a);
+            //Prueba del archivo
+            File f = new File("grafo.jpg");
+            String ruta = f.getPath();
+            //Colocar imagen
+            File file = new File(ruta);
+            Image image = new Image(file.toURI().toString());
+            imgAutomata.setImage(image);
             String texto = txtCadena.getText();
             if (Pattern.matches(a, texto) == true) {
                 //Mensaje de que la palabra es correcta
@@ -112,7 +136,54 @@ public class FXMLDocumentController implements Initializable {
             Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
     //<------------------------------------------------------------------------>
+    @FXML
+    public void expresion(String expresion1) {
+        RegExp r = new RegExp(expresion1);
+        Automaton aut = r.toAutomaton();
+        String grafo = aut.toDot();
+        System.out.println(grafo);
+        creararchivo(grafo);
+    }
+
+    public static void creararchivo(String grafo) {
+        borrar(imagen);
+        borrar(textoBase);
+        try {
+            FileWriter archivo = new FileWriter(textoBase);
+            PrintWriter pw = new PrintWriter(archivo);
+            grafo = grafo.replace("u0020", "Ɛ");
+            pw.write(grafo);
+            archivo.close();
+            grafo = "";
+            creaImagen();
+        } catch (IOException e) {
+            System.err.println("Error: " + e.toString());
+        }
+    }
+
+    public static void borrar(String path) {
+        File archivo = new File(path);
+        archivo.delete();
+    }
+
+    public static void creaImagen() {
+        try {
+            String[] cmd = new String[5];
+            cmd[0] = rutaDot;
+            cmd[1] = jp;
+            cmd[2] = textoBase;
+            cmd[3] = O;
+            cmd[4] = imagen;
+
+            Runtime rt = Runtime.getRuntime();
+            rt.exec(cmd);
+        } catch (IOException ex) {
+            System.out.println("IOException -> " + ex);
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
